a11d2b2589098c66e139b33a4c4a28f8e4f9a8e0
Naseus <tdevries4i@gmail.com>
2019-09-17 10:44:12 -0400

    update_initDb

--- initDb.php
+++ initDb.php
@@ -1,4 +1,20 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require('config.php');
-echo $ host;
+echo "Loaded Host: " . $host;
+
+//TODO add error handling
+
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+try{
+	$db = new PDO($conn_string, $username, $password);
+	echo "Connected";
+}
+catch(Exception $e){
+	echo $e->getMessage();
+	exit("Something went wrong");
+}
 ?>
