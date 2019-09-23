fa7f03be59acce25fefe5fee81b0e709d320644a
Naseus <tdevries4i@gmail.com>
2019-09-23 14:59:47 -0400

    init

--- initDb.php
+++ initDb.php
@@ -25,9 +25,9 @@ try{
 
 $insert_query = "INSERT INTO `TestUsers`(`username`,`pin`) VALUES(:username, :pin)";
 $stmt = $db->prepare($insert_query);
-$newUser = "JohnDoe";
-$newPin = 1234;
-$r = $stmt->execute(array->(":username"=>$newUser,":pin"=>$newPin));
+$newUser = "Billy";
+$newPin = 1142;
+$r = $stmt->execute(array(":username"=>$newUser,":pin"=>$newPin));
 print_r($stmt->errorInfo());
 
 //select * from TestUsers where username =
@@ -36,7 +36,7 @@ $stmt = $db ->prepare($select_query);
 $r = $stmt-> execute(array(":username"=>"Billy"));
 $results = $stmt ->fetch(PDO::FETCH_ASSOC);
 print_r($stmt->errorInfo());
-echo"<pre>" . var_export($results,true) . <pre>"; 
+echo"<pre>" . var_export($results,true) . "<pre>"; 
 }
 catch(Exception $e){
 	echo $e->getMessage();
