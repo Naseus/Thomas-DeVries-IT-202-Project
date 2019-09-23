e2b66fe722d45e964eabbd44024af7d4f5a18c92
Naseus <tdevries4i@gmail.com>
2019-09-23 14:38:20 -0400

    init

--- initDb.php
+++ initDb.php
@@ -20,7 +20,23 @@ try{
 		) CHARACTER SET utf8 COLLATE utf8_general_ci";
 	$stmt = $db->prepare($query);
 	$r = $stmt->execute();
-	echo "<br>" . $r . "<br>";
+	echo "<br>" . ($r>0?"Created table or already exists":"Failed to create table") . "<br>";
+	unset($r);
+
+$insert_query = "INSERT INTO `TestUsers`(`username`,`pin`) VALUES(:username, :pin)";
+$stmt = $db->prepare($insert_query);
+$newUser = "JohnDoe";
+$newPin = 1234;
+$r = $stmt->execute(array->(":username"=>$newUser,":pin"=>$newPin));
+print_r($stmt->errorInfo());
+
+//select * from TestUsers where username =
+$select_query = "select * from `TestUsers` where username = :username";
+$stmt = $db ->prepare($select_query);
+$r = $stmt-> execute(array(":username"=>"Billy"));
+$results = $stmt ->fetch(PDO::FETCH_ASSOC);
+print_r($stmt->errorInfo());
+echo"<pre>" . var_export($results,true) . <pre>"; 
 }
 catch(Exception $e){
 	echo $e->getMessage();
