c473d71cbaa93b9e49c9fa2efb3929e7b0ce336d
Naseus <tdevries4i@gmail.com>
2019-10-15 01:08:29 -0400

    Handling

---
+++ dynamicHandling.php
@@ -0,0 +1,19 @@
+<?php
+<html>
+<head>
+</head>
+<body>
+<form onsubmit="return false;">
+
+<input name="email" type="email"/>
+<input name="emailconfirm" type="email"/>
+<input name="password" type="password"/>
+<input name="passwordconfirm" type="password"/>
+<input name="username"/>
+
+<input type="submit" value="Submit"/>
+
+</form>
+</body>
+</html>
+?>
