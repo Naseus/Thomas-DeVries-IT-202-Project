cbd7d1ecf441f0994e246ad6d0df159f9ebfc12f
Naseus <tdevries4i@gmail.com>
2019-10-31 15:21:53 -0400

    "test response"

--- login.php
+++ login.php
@@ -1,10 +1,5 @@
 <html>
 <head>
-<script>
-function handleUser() {
-	
-}
-</script>
 <style>
 input{ border: 1px solid black; border-radius: 2px; margin-left: auto; margin-right:auto; padding 1%;}
 H1 {font-family:Arial; font-size: 20px; margin-left: auto; margin-right:auto;}
@@ -13,8 +8,8 @@ H1 {font-family:Arial; font-size: 20px; margin-left: auto; margin-right:auto;}
 <body>
 <H1>Login</H1>
 <HR>
-<form onsubmit = "handleUser();">
-<input name="username"  placeholder = "username" style width = 300px/>
+<form onsubmit = "<?php handleUser(); ?>">
+<input name="username"  placeholder = "username" style width = 300px onchange="validate(this);" />
 <br>
 <input name="email" type="email" placeholder = "email@domain.com"/>
 <input name="emailconfirm" type="email"  placeholder = "email@domain.com"/>
@@ -22,7 +17,30 @@ H1 {font-family:Arial; font-size: 20px; margin-left: auto; margin-right:auto;}
 <input name="password" type="password"  placeholder = "password"/>
 <input name="passwordconfirm" type="password"  placeholder = "confirm password"/>
 <br>
-<input type="submit" value="Submit"/>
+<input type="submit" value="Login"/>
 </form>
 </body>
 </html>
+
+<?php
+function getSampleUsers() {
+	require('config.php');
+	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+	try {
+		$db = new PDO($conn_string, $username, $password);
+		$select_qurey = "select id, username from `TestUsers`";
+		$r = $stmt->execute();
+		$response = json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+	}
+	 catch(Exception $e){
+		$response = "DB error: $e";
+	}
+	return $response;
+}
+
+function handleUsers() {
+	$response = getSampleUsers();
+		echo "<br>" . $response . "<br>";
+
+} 
+?>
