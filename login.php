152d1e38801b049bfaee8ecbbbe593a0b0a70316
Naseus <tdevries4i@gmail.com>
2019-10-31 15:01:03 -0400

    Sublime commit

---
+++ login.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<script>
+function handleUser() {
+	
+}
+</script>
+<style>
+input{ border: 1px solid black; border-radius: 2px; margin-left: auto; margin-right:auto; padding 1%;}
+H1 {font-family:Arial; font-size: 20px; margin-left: auto; margin-right:auto;}
+</style>
+</head>
+<body>
+<H1>Login</H1>
+<HR>
+<form onsubmit = "handleUser();">
+<input name="username"  placeholder = "username" style width = 300px/>
+<br>
+<input name="email" type="email" placeholder = "email@domain.com"/>
+<input name="emailconfirm" type="email"  placeholder = "email@domain.com"/>
+<br>
+<input name="password" type="password"  placeholder = "password"/>
+<input name="passwordconfirm" type="password"  placeholder = "confirm password"/>
+<br>
+<input type="submit" value="Submit"/>
+</form>
+</body>
+</html>
