dc2daaa82f61588e30e1f59a9bd20657e9535172
Naseus <tdevries4i@gmail.com>
2019-10-31 20:28:35 -0400

    fix validation

--- login.php
+++ login.php
@@ -4,7 +4,6 @@
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 function handleUsers() {
-	return True;
 	require('config.php');
 	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 	try {
@@ -29,9 +28,10 @@ function handleUsers() {
 		function validate() {
 			let rtn = true;
 			let form = document.forms[0];
-			if(form.username == "") 
+			console.log(form.username);			
+			if(form.username.value == "") 
 				rtn = false;
-			if(form.password != form.passwordconfirm) 
+			if(form.password.value != form.passwordconfirm.value) 
 				rtn = false;
 			if(rtn)
 				console.log("Succsess");
@@ -46,6 +46,7 @@ function handleUsers() {
 </head>
 <body>
 	<H1>Login</H1>
+	<HR>
 	<form method = 'post' action = '#' onsubmit= "return validate();">
 		<input name="username"  placeholder = "username" style width = "300px"/>
 		<br>
@@ -53,9 +54,9 @@ function handleUsers() {
 		<input name="passwordconfirm" type="password"  placeholder = "confirm password"/>
 		<br>
 		<input type="submit" value="Login"/>
-	</form>
 </body>
 </html>
 <?php handleUsers();?>
 <?php echo "<br><pre>" . var_export($_POST, true) . "</pre><br>";?>
 
+	</form>
