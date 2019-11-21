c3303185d3bb385aa1600ea4ca65c3344adea665
Naseus <tdevries4i@gmail.com>
2019-11-21 15:06:13 -0500

    Fixing file issues

--- LastLayer.php
+++ LastLayer.php
@@ -1,14 +1,16 @@
 <?php
 start_session();
 if(isset($_SESSION['user'])){
-	echo $_SESSION['user'] . " time for database stuff" ;
+	echo $_SESSION['user'] . " time for database stuff";
 } else {
 	exit();
 }
 ?>
 <html>
 <body>
+	<br>
 	<a href="Landing.php">OLL Lab</a>
+	<br>
 	<a href="Landing.php">PLL</a>
 </body>
 </html>
