83c202558bfa1986ddfa54a5a94b3edb285b8ae7
Naseus <tdevries4i@gmail.com>
2019-11-23 15:17:35 -0500

    Fixing id's

--- AlgorithmLab.php
+++ AlgorithmLab.php
@@ -60,7 +60,7 @@ function addAlg($algoritham, $length, $name) {
         			text: n,
         			id: 'btn_'+ n,
         			click: function () { 
-        					$("#textArea span").text($("#textArea span").text() + n);
+        					$("#textArea").text($("#textArea").text() + n);
         					 }
     					});
      		$(b).appendTo($('#buttons'));
