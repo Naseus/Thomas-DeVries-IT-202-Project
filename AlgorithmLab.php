98b9d43a32a65562a00488b2d1d3584af325765a
Naseus <tdevries4i@gmail.com>
2019-11-23 14:41:47 -0500

    fix

--- AlgorithmLab.php
+++ AlgorithmLab.php
@@ -60,7 +60,7 @@ function addAlg($algoritham, $length, $name) {
         			text: n,
         			id: 'btn_'+ n,
         			click: function () { 
-        					document.textArea.innerText += n; }
+        					$('#textArea').innerText += n; }
     					});
      		console.log(b[0]);
      		console.log($(this));
