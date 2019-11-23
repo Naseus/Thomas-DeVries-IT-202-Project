9903f1cea2542483026cb8b2cbd4a05df4bc15dc
Naseus <tdevries4i@gmail.com>
2019-11-23 15:00:46 -0500

    Trying html

--- AlgorithmLab.php
+++ AlgorithmLab.php
@@ -60,7 +60,7 @@ function addAlg($algoritham, $length, $name) {
         			text: n,
         			id: 'btn_'+ n,
         			click: function () { 
-        					$("#textArea").text += n;
+        					$("#textArea").html += n;
         					console.log($("#textArea").text);
         					console.log($("#textArea").html);
         					 }
