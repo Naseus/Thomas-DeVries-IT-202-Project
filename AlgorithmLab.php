2225ff2c2ee23d03a625b750c4e0ec40c245849e
Naseus <tdevries4i@gmail.com>
2019-11-23 16:20:01 -0500

    print database name

--- AlgorithmLab.php
+++ AlgorithmLab.php
@@ -31,7 +31,7 @@ function addAlg() {
 		$algoritham = $_POST["algValue"];
 		$length = count(str_word_count($_POST["algValue"], 1));
 		$name = $_POST["name"];
-		echo ($length);
+		echo ($algDatabase);
 		echo ($_POST["algValue"]);
 		$stmt =$db->prepare("INSERT into $algDatabase (`alg_name`,`alg`,`move_number`) VALUES (:name, :alg, :length)");
 		$run = $stmt->execute(array(
