95a19d8af20b1f9f82b6af8ee7b24c610f1a2d32
Naseus <tdevries4i@gmail.com>
2019-11-26 11:10:25 -0500

    Testing href

--- PLL.php
+++ PLL.php
@@ -3,5 +3,15 @@
 ?>
 
 <html>
-	<a href="LastLayer.php">Back</a>
+	<head>
+		<script>
+			function test(let x) {
+				alert(x);
+			}
+		</script>
+	</head>
+	<body>
+		<a href="javascript: alert('RAN');">Back</a>
+		<a href="LastLayer.php">Back</a>
+	</body>
 </html>
