81f29a94a87d782e5f626ee8f1af5421572e2c24
Naseus <tdevries4i@gmail.com>
2019-12-06 13:30:18 -0500

    var_export

--- PLL.php
+++ PLL.php
@@ -37,7 +37,7 @@
 	echo $rtn;
 }
 //	HANDLES THE DATA SUMBITED TO THE BACKEND
-echo "<br><pre>" . $_POST . "</pre><br>";
+echo "<br><pre>" . var_export($_POST) . "</pre><br>";
 if(isset($_POST["add"]) && isset($_GET['alg'])) {
 	addAlg();
 }
